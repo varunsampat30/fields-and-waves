75ba18467f31064b7b488d025c63a85cd0caa033
maidawil <93547307+maidawil@users.noreply.github.com>
2021-11-05 13:35:36 -0400

    Delete UnknownMicrostripCode.m

--- UnknownMicrostripCode.m
+++
@@ -1,7 +0,0 @@
-data=csvread('unknownMicrostrip.csv');
-x=data(:,1);
-y=data(:,2);
-plot(x,y);
-xlabel('Distance From Maximum (cm)');
-ylabel('Amplitude at 1GHz (dB)');
-title('Unknown Microstrip');
