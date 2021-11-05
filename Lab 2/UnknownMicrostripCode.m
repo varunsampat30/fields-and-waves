a3ed288ea0a3bbd7b71542164268b8d577c39aae
maidawil <93547307+maidawil@users.noreply.github.com>
2021-11-05 13:33:53 -0400

    Add files via upload

---
+++ Lab 2/UnknownMicrostripCode.m
@@ -0,0 +1,10 @@
+data=csvread('unknownMicrostrip.csv');
+x=data(:,1);
+y=data(:,2);
+h=figure;
+plot(x,y);
+xlabel('Distance From Maximum (cm)');
+ylabel('Amplitude at 1GHz (dB)');
+title('Unknown Load Microstrip');
+saveas(h,'Unknown Load Microstrip.pdf');
+close (h);
