282f0fff97a57ef6ac9fd72134cc435f3d118866
varunsampat30 <varunsampat30@gmail.com>
2021-11-13 10:56:55 -0500

    Created script for experimental Z0

---
+++ Lab 2/experimental_Z_0.m
@@ -0,0 +1,14 @@
+reference_point = 38.4
+% d_min = linspace(0, 20, 41)
+d_min = 32.9
+d_min = (reference_point - d_min)*(10^-2)
+lambda = 0.12
+abs_gamma = 0.2425934462
+z0 = 50
+
+theta = pi + (4*pi*d_min)/lambda
+
+gamma = abs_gamma * exp(i*theta)
+
+zl = -z0 * (gamma + 1)/(gamma-1)
+
